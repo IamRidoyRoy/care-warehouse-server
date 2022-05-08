2e571dc6d0245299e8e0369935911e5cf64ff18a
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-09 01:31:09 +0600

    DB is connected

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Listening to port', port)
 })
+
+
+// Connect with mongoDB
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.az3oh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    console.log("DB connected")
+});
